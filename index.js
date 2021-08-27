d23bb79f838d86f9bcc4d9ce02b9fb1986a2d24f
odicho <amanuelodicho@gmail.com>
2021-08-26 22:25:13 -0500

    first heroku

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
 import cors from "cors";
 import queuesRoutes from "./routes/queues.js";
 import mongoose from "mongoose";
@@ -26,10 +25,6 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/queues", queuesRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
