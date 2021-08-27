85b51a683e8841acb88f0a5d7e03f96a6c380b10
odicho <amanuelodicho@gmail.com>
2021-08-26 20:16:40 -0500

    first heroku

--- index.js
+++ index.js
@@ -18,11 +18,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`);
 
 mongoose
   .connect(process.env.mongodb)
-  //   .then(() =>
-  //     app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
-  //   )
   .catch((err) => console.log(err));
