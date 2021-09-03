e1fa47d808b8867c188b3385a8378eaba3e602c1
Amanuel Odicho <amanuelodicho@example.com>
2021-09-02 21:43:44 -0500

    forgot to add dependecy for delete http request method

--- routes/deletedQueues.js
+++ routes/deletedQueues.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   readDeletedQueues,
   createDeletedQueue,
+  deleteDeletedQueue,
 } from "../controller/deletedQueues.js";
 
 const router = express.Router();
 router.get("/", readDeletedQueues);
 router.post("/:id", createDeletedQueue);
+router.delete("/:id", deleteDeletedQueue);
 // router.patch("/:id", updateQueue);
 // router.delete("/:id", deleteQueue);
 
