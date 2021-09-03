4866c83f2110a207aad836a0abbdc96f9ddc54e5
Amanuel Odicho <amanuelodicho@example.com>
2021-09-03 00:20:36 -0500

    removed toggle feature to document model

--- models/queues.js
+++ models/queues.js
@@ -10,10 +10,6 @@ const queueSchema = new Schema(
       type: String,
       required: true,
     },
-    isToggled: {
-      type: Boolean,
-      required: true,
-    },
   },
   { timestamps: true }
 );
